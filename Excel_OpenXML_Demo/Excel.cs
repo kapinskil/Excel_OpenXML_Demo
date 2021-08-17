e67d67a1b8451c58cac024423455a77b5668c5c7
Łukasz Kapiński <kapinski.l@gmail.com>
2021-08-17 22:42:44 +0200

    Add project files.

---
+++ Excel_OpenXML_Demo/Excel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace Excel_OpenXML_Demo
+{
+    class Excel
+    {
+        public static void CreateWorkbook(string path)
+        {
+            var spreadsheetDocument = SpreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook);
+
+            var workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+
+            Sheet sheet = new Sheet() { Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "mySheet" };
+
+            sheets.Append(sheet);
+
+            workbookpart.Workbook.Save();
+
+            spreadsheetDocument.Close();
+        }
+    }
+}
---
+++ Excel_OpenXML_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excel_OpenXML_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.CreateWorkbook(@"C:\Users\kapin\OneDrive\Pulpit\test.xlsx");
+        }
+    }
+}
